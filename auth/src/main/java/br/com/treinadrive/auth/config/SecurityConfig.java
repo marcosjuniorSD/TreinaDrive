de6c032d623fd244104a34ad099bcff77b625baa
marcosjuniorSD <projetopessoalbr@gmail.com>
2025-06-14 13:11:46 -0300

    fix(auth) fixing public endpoint access.

--- auth/src/main/java/br/com/treinadrive/auth/config/SecurityConfig.java
+++ auth/src/main/java/br/com/treinadrive/auth/config/SecurityConfig.java
@@ -1,8 +1,10 @@
 package br.com.treinadrive.auth.config;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.config.annotation.web.configurers.oauth2.server.resource.OAuth2ResourceServerConfigurer;
 import org.springframework.security.web.SecurityFilterChain;
 
@@ -17,8 +19,11 @@ public class SecurityConfig {
     private static final String INSTRUCTOR = "instructor";
     private static final String STUDENT = "student";
 
+    @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
-        http.authorizeHttpRequests(auth -> auth
+        http
+                .csrf(AbstractHttpConfigurer::disable)
+                .authorizeHttpRequests(auth -> auth
                 .requestMatchers(PUBLIC_ROUTE).permitAll()
                 .requestMatchers(INSTRUCTOR_ROUTE).hasRole(INSTRUCTOR)
                 .requestMatchers(STUDENT_ROUTE).hasRole(STUDENT)
